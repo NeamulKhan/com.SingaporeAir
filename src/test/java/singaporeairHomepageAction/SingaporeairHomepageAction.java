546ccb227feecae96e025e2bad2a32c2a897e63d
Neamul Khan <neamulkhan@yahoo.com>
2023-12-26 13:33:46 -0500

    added dept and arrive destination

--- src/test/java/singaporeairHomepageAction/SingaporeairHomepageAction.java
+++ src/test/java/singaporeairHomepageAction/SingaporeairHomepageAction.java
@@ -23,6 +23,23 @@ public class SingaporeairHomepageAction extends BaseClassSingaporeAir{
 		
 	}
 	
-	
+	public void click_bookflights_radiobutton_enter_departcity_click_correct_popup_airport() throws Exception {
+		
+		Thread.sleep(3000);
+		
+		singaporeairHomepageLocator.bookflightsradiobutton.click();
+		Thread.sleep(2000);
+		singaporeairHomepageLocator.crossicon.click();
+		Thread.sleep(2000);
+		singaporeairHomepageLocator.departcity.sendKeys("Toronto");
+		singaporeairHomepageLocator.suggesteddepartcity.click();
+				
+	}
+	public void enter_arrivalcity_and_click_correct_popup_airport() throws Exception {
+		Thread.sleep(3000);
+		singaporeairHomepageLocator.arrivalcity.sendKeys("Dhaka");
+		singaporeairHomepageLocator.suggestedarrivcity.click();
+		
+	}
 	
 }
--- src/test/java/singaporeairHomepageLocator/SingaporeairHomepageLocator.java
+++ src/test/java/singaporeairHomepageLocator/SingaporeairHomepageLocator.java
@@ -17,5 +17,24 @@ public class SingaporeairHomepageLocator extends BaseClassSingaporeAir{
 	@FindBy (xpath="//*[@id=\"hwidget\"]/div[2]/div/div[2]/div[1]")
 	public WebElement flightbookingwindow;
 	
+	@FindBy (xpath="//*[@id=\"hwidget\"]/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/div[2]/div/div/div[1]/div[1]/label")
+	public WebElement bookflightsradiobutton;
+	
+	@FindBy (xpath="//*[@id=\"hwidget\"]/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/form/div[1]/div[1]/div/div/div[1]/div/div/span[2]")
+	public WebElement crossicon;
+	
+	@FindBy (id="flightOrigin1")
+	public WebElement departcity;
+	
+	@FindBy (xpath="//*[@id=\"hwidget\"]/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/form/div[1]/div[1]/div/div/div[1]/div/div[2]/div/div/div/div[1]/div[1]")
+	public WebElement suggesteddepartcity;
+	
+	@FindBy (id="bookFlightDestination")
+	public WebElement arrivalcity;
+	
+	@FindBy (xpath="//*[@id=\"hwidget\"]/div[2]/div/div[2]/div[1]/div/div/div[3]/div[2]/form/div[1]/div[1]/div/div/div[2]/div/div[2]/div/div/div/div[1]/div[1]")
+	public WebElement suggestedarrivcity;
+	
+	
 
 }
